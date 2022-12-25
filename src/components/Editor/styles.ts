0ff100d4ddc04c890c5aa4b49c937e8dc36107ed
nodari-dev <nodari.pylypyshak@gmail.com>
2022-12-25 23:29:21 +0100

    simple render, compare parts

--- src/components/Editor/styles.ts
+++ src/components/Editor/styles.ts
@@ -1,10 +1,28 @@
-import {createUseStyles} from 'react-jss'
+import { createUseStyles } from 'react-jss';
 
-const styles = createUseStyles ({
+const styles = createUseStyles({
     editor: {
         border: '1px solid red',
-        width: "100%"
-    }
-})
+        width: '100%',
+    },
+    line: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        border: '1px solid blue',
+
+        '&[data-edit-leaf="true"]': {
+            '&::after': {
+                content: "''",
+                display: 'inline-flex',
+                alignSelf: 'center',
+                position: 'relative',
+                height: 15,
+                width: 1,
+                background: 'red',
+            },
+        },
+    },
+});
 
 export default styles;
